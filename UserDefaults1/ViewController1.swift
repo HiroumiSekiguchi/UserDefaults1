3c05768b7d438d74df8e04b071a0134a9cafac93
eHime <kinuyamakomaba@gmail.com>
2019-06-26 23:16:43 +0900

    Initial commitment! This step was pretty easy!

---
+++ UserDefaults1/ViewController1.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController1.swift
+//  UserDefaults1
+//
+//  Created by 関口大海 on 2019/06/26.
+//  Copyright © 2019 関口大海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UITableViewController {
+    
+    
+    // セルに表示する項目を格納
+    var itemArray = ["本を読む", "映画見る", "夕飯を買う"]
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.clearsSelectionOnViewWillAppear = false
+    }
+    
+    
+    
+    // ☆☆☆以下、TableViewについての設定☆☆☆ //
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // タップしたセルを離した後に非選択に戻す
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // タップしたセルのチェックを付けたり外したりする
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+    
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

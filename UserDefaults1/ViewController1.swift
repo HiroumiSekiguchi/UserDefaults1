c40bb436f3302e88cf3670787e3a7c699fd148a8
eHime <kinuyamakomaba@gmail.com>
2019-06-27 18:03:20 +0900

    セルのReusable問題に対処する前のコード（.checkmarkなどaccessoryTypeがReuseされて下の方のセルで上のセルのものが複製されてしまっている状況）

--- UserDefaults1/ViewController1.swift
+++ UserDefaults1/ViewController1.swift
@@ -22,7 +22,7 @@ class ViewController1: UITableViewController {
         super.viewDidLoad()
         self.clearsSelectionOnViewWillAppear = false
         
-        // UD内のデータをitemArrayに反映（UD内にデータがなかった場合にクラッシュしないように条件分岐で守る）
+//         UD内のデータをitemArrayに反映（UD内にデータがなかった場合にクラッシュしないように条件分岐で守る）
         if let items = defaults.array(forKey: "items") as? [String] {
             itemArray = items
         }
@@ -99,52 +99,5 @@ class ViewController1: UITableViewController {
         }
         
     }
-    
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
